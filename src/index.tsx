e0e2b7b649dc5f518ffc3b529e25ae8e7bb595ae
Nikita Kozloff <hello.kozloff@gmail.com>
2021-03-11 00:56:41 +0300

    <feature/index> add theme provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
+import theme from './theme';
+
 import { InputSearch } from './components/organisms';
 
 function WelcomeScreen(): React.ReactElement {
   return (
-    <InputSearch />
+    <ThemeProvider theme={theme}>
+      <InputSearch />
+    </ThemeProvider>
   );
 }
 
