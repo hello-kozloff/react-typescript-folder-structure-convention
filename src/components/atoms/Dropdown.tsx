eafae41268a39d57f769b691882cc97d9779e4ef
Nikita Kozloff <hello.kozloff@gmail.com>
2021-03-11 00:44:59 +0300

    <chore/atom> relocate styles

--- src/components/atoms/Dropdown.tsx
+++ src/components/atoms/Dropdown.tsx
@@ -36,48 +36,6 @@ export interface IDropdownProps {
 
 }
 
-const Anchor = styled.div`
-  position: relative;
-  display: inline-block;
-`;
-
-const Content = styled.div<IDropdownProps>`
-  position: absolute;
-  z-index: 999;
-  
-  ${({ side, gap }) => {
-    switch (side) {
-      case 'top': return css`
-        bottom: 100%;
-        padding-bottom: ${gap}px;
-      `;
-      case 'right': return css`
-        left: 100%;
-        padding-left: ${gap}px;
-      `;
-      case 'bottom': return css`
-        top: 100%;
-        padding-top: ${gap}px;
-      `;
-      case 'left': return css`
-        right: 100%;
-        padding-right: ${gap}px;
-      `;
-    }
-  }};
-  
-  ${({ align }) => {
-    switch (align) {
-      case 'left': return css`
-        left: 0;
-      `;
-      case 'right': return css`
-        right: 0;
-      `;
-    }
-  }};
-`;
-
 /**
  * The dropdown atom.
  * @param props
@@ -85,6 +43,48 @@ const Content = styled.div<IDropdownProps>`
  */
 export default function Dropdown(props: IDropdownProps): React.ReactElement<IDropdownProps> {
   const { anchor, children: content } = props;
+
+  const Anchor = styled.div`
+    position: relative;
+    display: inline-block;
+  `;
+
+  const Content = styled.div<IDropdownProps>`
+    position: absolute;
+    z-index: 999;
+    
+    ${({ side, gap }) => {
+      switch (side) {
+        case 'top': return css`
+          bottom: 100%;
+          padding-bottom: ${gap}px;
+        `;
+        case 'right': return css`
+          left: 100%;
+          padding-left: ${gap}px;
+        `;
+        case 'bottom': return css`
+          top: 100%;
+          padding-top: ${gap}px;
+        `;
+        case 'left': return css`
+          right: 100%;
+          padding-right: ${gap}px;
+        `;
+      }
+    }};
+    
+    ${({ align }) => {
+      switch (align) {
+        case 'left': return css`
+          left: 0;
+        `;
+        case 'right': return css`
+          right: 0;
+        `;
+      }
+    }};
+  `;
 
   const defaultProps = {
     anchor,
