7412a52643568248ac85b1a0b3caceccf59209df
Nikita Kozloff <hello.kozloff@gmail.com>
2021-03-11 13:49:15 +0300

    <feature/styles> create main styles

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
 import theme from './theme';
+import './styles/main.scss';
 
 import { InputSearch } from './components/organisms';
 
