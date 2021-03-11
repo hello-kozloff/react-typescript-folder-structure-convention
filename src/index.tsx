c79f5d8d8b0812b8906c04c4dd1f59ab840a6623
Nikita Kozloff <hello.kozloff@gmail.com>
2021-03-11 21:24:11 +0300

    <chore/index> upgrade render

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,24 @@ import { ThemeProvider } from 'styled-components';
 import theme from './theme';
 import './styles/main.scss';
 
-import { InputSearch } from './components/organisms';
+import { Input, Column } from './components/atoms';
+import { InputContainer, Row } from './components/molecules';
 
 function WelcomeScreen(): React.ReactElement {
-  return <InputSearch />;
+  return (
+    <Row gap={30}>
+      <Column>
+        <InputContainer before="Промокод" after="Оставьте поле пустым, если бессрочно" required>
+          <Input stretched />
+        </InputContainer>
+      </Column>
+      <Column>
+        <InputContainer before="Промокод" after="Оставьте поле пустым, если бессрочно" required>
+          <Input stretched />
+        </InputContainer>
+      </Column>
+    </Row>
+  );
 }
 
 ReactDOM.render((
