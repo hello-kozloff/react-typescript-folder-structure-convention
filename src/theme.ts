29b60e8977c52195cc08e86e6045a8d1e6477d47
Nikita Kozloff <hello.kozloff@gmail.com>
2021-03-11 14:46:00 +0300

    <chore/theme> rename colors

--- src/theme.ts
+++ src/theme.ts
@@ -4,19 +4,28 @@ const theme: Theme = {
   dark: {
     color: {
       white: 'rgb(255, 255, 255)',
-      white_80: 'rgba(255, 255, 255, .8)',
-      white_40: 'rgba(255, 255, 255, .4)',
-      white_30: 'rgba(255, 255, 255, .3)',
+      white_80: 'rgba(255, 255, 255, .80)',
+      white_40: 'rgba(255, 255, 255, .40)',
+      white_30: 'rgba(255, 255, 255, .30)',
       white_15: 'rgba(255, 255, 255, .15)',
-      white_08: 'rgba(255, 255, 255, .8)',
+      white_08: 'rgba(255, 255, 255, .08)',
       //
       primary: 'rgb(50, 100, 239)',
       //
       black: 'rgb(35, 35, 35)',
-      black_05: 'rgba(35, 35, 35, .5)',
+      black_90: 'rgba(35, 35, 35, .90)',
+      black_40: 'rgba(35, 35, 35, .40)',
+      black_30: 'rgba(35, 35, 35, .30)',
+      black_15: 'rgba(35, 35, 35, .15)',
+      black_07: 'rgba(35, 35, 35, .07)',
+      black_05: 'rgba(35, 35, 35, .05)',
       //
       dark: 'rgb(48, 48, 48)',
-      dark_05: 'rgba(48, 48, 48, .5)',
+      dark_90: 'rgba(48, 48, 48, .90)',
+      dark_40: 'rgba(48, 48, 48, .40)',
+      dark_30: 'rgba(48, 48, 48, .30)',
+      dark_15: 'rgba(48, 48, 48, .15)',
+      dark_05: 'rgba(48, 48, 48, .05)',
     },
     transition: {
       fast: '.15s ease-out',
