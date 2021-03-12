d66b9c1b2be4347c10f11ce66b044e062e3061b7
Nikita Kozloff <hello.kozloff@gmail.com>
2021-03-12 19:37:05 +0300

    <chore/helper> remove outline

--- src/helper/clearInput.ts
+++ src/helper/clearInput.ts
@@ -1,12 +1,17 @@
 import { css } from 'styled-components';
 
 const clearInput = css`
+  outline: none;
   resize: none;
   padding: 0;
   border: none;
   background: transparent;
   font: inherit;
   color: inherit;
+  
+  &:focus::placeholder {
+    opacity: 0;
+  }
 `;
 
 export default clearInput;
