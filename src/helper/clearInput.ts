8206a0a110d81dccdc3a36908e5864631b32aae4
Nikita Kozloff <hello.kozloff@gmail.com>
2021-03-12 19:32:37 +0300

    <feature/helper> add clearInput

---
+++ src/helper/clearInput.ts
@@ -0,0 +1,12 @@
+import { css } from 'styled-components';
+
+const clearInput = css`
+  resize: none;
+  padding: 0;
+  border: none;
+  background: transparent;
+  font: inherit;
+  color: inherit;
+`;
+
+export default clearInput;
--- src/helper/index.ts
+++ src/helper/index.ts
@@ -1,1 +1,2 @@
 export { default as getDefaultProps } from './getDefaultProps';
+export { default as clearInput } from './clearInput';
