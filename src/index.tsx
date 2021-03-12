3b1c1af25a0c3349440c9bfbcb4f8e0a58aec561
Nikita Kozloff <hello.kozloff@gmail.com>
2021-03-13 00:19:34 +0300

    <chore/index> update render

--- src/index.tsx
+++ src/index.tsx
@@ -4,26 +4,28 @@ import { ThemeProvider } from 'styled-components';
 import theme from './theme';
 import './styles/main.scss';
 
-import { Inline, Margin, Image, InputTitle, InputDescription } from './components/atoms';
+import { Inline, Padding, Margin, Image, InputTitle, InputDescription } from './components/atoms';
 import { UploadImage } from './components/molecules';
 
 function CourseHeader(): React.ReactElement {
   return (
-    <Inline align="flex-start" stretched>
-      <Margin size={[0, 30, 0, 0]}>
-        <Inline direction="column">
-          <UploadImage radius="5px">
-            <Image width="300px" height="170px" alt="Изображение курса" />
-          </UploadImage>
-        </Inline>
-      </Margin>
-      <Inline direction="column" stretched>
-        <InputTitle />
-        <Margin size={[16, 0, 0, 0]}>
-          <InputDescription />
+    <Padding size={[40, 40, 40, 40]}>
+      <Inline align="flex-start" stretched>
+        <Margin size={[0, 30, 0, 0]}>
+          <Inline direction="column">
+            <UploadImage radius="5px">
+              <Image width="300px" height="170px" alt="Изображение курса" />
+            </UploadImage>
+          </Inline>
         </Margin>
+        <Inline direction="column" stretched>
+          <InputTitle />
+          <Margin size={[16, 0, 0, 0]}>
+            <InputDescription />
+          </Margin>
+        </Inline>
       </Inline>
-    </Inline>
+    </Padding>
   );
 }
 
