3d62f632c9f094049587c133f25c9a44c8a3e33f
Nikita Kozloff <hello.kozloff@gmail.com>
2021-03-14 01:15:31 +0300

    <feature/router> create router

---
+++ src/router.ts
@@ -0,0 +1,15 @@
+import createRouter from 'router5';
+import loggerPlugin from 'router5-plugin-logger';
+import browserPlugin from 'router5-plugin-browser';
+import routes from './routes';
+
+export default function configureRouter() {
+  const router = createRouter(routes, {
+    defaultRoute: 'course.add'
+  });
+
+  router.usePlugin(loggerPlugin);
+  router.usePlugin(browserPlugin());
+
+  return router.start();
+}
